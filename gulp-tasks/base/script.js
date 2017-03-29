ce7fb63cf25bd38e5a8ee2393e2bc6cbc3a1a38d
FanJunfang <FanJunfang>
2017-03-29 13:30:28 +0800

    [ TASK ]  WEBWDT-6  ES6和react编译、JS Modernizr、压缩及合并

--- gulp-tasks/base/script.js
+++ gulp-tasks/base/script.js
@@ -1,21 +1,23 @@
 const fs   = require('fs');
-const path = require('path');
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
 const browserify = require('browserify');
 const babelify = require('babelify');
 const es2015 = require('babel-preset-es2015');
 const react = require('babel-preset-react');
 const source = require('vinyl-source-stream');
 const buffer = require('vinyl-buffer');
+const modernizr = require('gulp-modernizr');
 const yaml = require('js-yaml');
+const jslint = require('gulp-jslint');
 
-js = () => {
+module.exports = function (gulp, callback){
     try {
-        const config = yaml.load(
+        const config = yaml.safeLoad(
             fs.readFileSync('config.yml', 'utf8')
         );
-        //const jsSrc = config.script.src;
+        const jsSrc = config.script.src;
         const jsDest = config.script.dest;
         return browserify("main.js")
            .transform(babelify, {
@@ -24,9 +26,14 @@ js = () => {
            .bundle()
            .pipe(source('bundle.js'))
            .pipe(buffer())
+           .pipe(jslint({
+               predef: [ 'a_global' ],
+               global: [ 'a_global' ]
+           }))
+           .pipe(modernizr())
+           .pipe(uglify())
            .pipe(gulp.dest(jsDest))
     } catch (e) {
         console.log(e);
     }
 }
-js();
