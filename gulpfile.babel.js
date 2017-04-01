832310a6b7c0c78626db7cd5a30b44df04e1f74a
FanJunfang <FanJunfang>
2017-04-01 09:32:51 +0800

    AddConnect

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import yaml from 'js-yaml';
 import gulpRequireTasks from 'gulp-require-tasks';
+import connect from 'gulp-connect';
 
 let configuration  = yaml.load(
             fs.readFileSync('config.yml', 'utf8')
@@ -8,5 +9,5 @@ let configuration  = yaml.load(
 
 gulpRequireTasks({
     path: process.cwd()+"/"+configuration.tasks.path,
-    arguments: [configuration]
+    arguments: [configuration,connect]
 });
