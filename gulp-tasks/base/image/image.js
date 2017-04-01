254af7af1e937951bae24327bbfb442a6ebe53ea
FanJunfang <FanJunfang>
2017-04-01 09:51:49 +0800

    DeleteNotes

--- gulp-tasks/base/image/image.js
+++ gulp-tasks/base/image/image.js
@@ -4,10 +4,10 @@ module.exports = {
     fn:function (gulp,configuration) {
         return gulp.src(configuration.image.path.src+"/*.{png,jpg,gif,ico}")
             .pipe(imagemin({
-                optimizationLevel:5, //类型：Number  默认：3  取值范围：0-7(优化等级)
-                interlaced: true, //类型：Boolean 默认：false 无损压缩jpg图片
-                progressive: true, //类型：Boolean 默认：false 隔行扫描gif进行渲染
-                multipass: true //类型：Boolean 默认：false 多次优化svg直到完全优化
+                optimizationLevel:5,
+                interlaced: true,
+                progressive: true,
+                multipass: true 
             }))
             .pipe(gulp.dest(configuration.image.path.dest))
     }
