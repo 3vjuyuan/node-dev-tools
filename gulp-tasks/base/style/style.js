a242d0bc212aa369b8dbcd25df110e4802462089
李妍 <liyan@3vjuyuan.com>
2017-04-01 15:10:12 +0800

    [TASK]:ScssCompileMinifyMerge

--- gulp-tasks/base/style/style.js
+++ gulp-tasks/base/style/style.js
@@ -3,14 +3,14 @@ import concat from 'gulp-concat';
 import csslint from 'gulp-csslint';
 
 module.exports = {
-    fn :function(gulp,configration,connect){
-        return gulp.src(configration.style.path.src+'/*.scss')
+    fn :function(gulp,configuration,connect){
+        return gulp.src(configuration.style.path.src+'/*.scss')
             .pipe(sass({
                 outputStyle: 'compressed'
             }))
             .pipe(csslint())
             .pipe(concat('app.min.css'))
-            .pipe(gulp.dest(configration.style.path.dest))
+            .pipe(gulp.dest(configuration.style.path.dest))
             .pipe(connect.reload());
     }
 }
