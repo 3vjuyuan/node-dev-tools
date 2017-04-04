1995e505193cc207a8babf86a9c696e17ac4f0a5
Weiye Sun <weiye.sun@3vjuyuan.com>
2017-04-04 22:35:34 +0200

    [TASK] Refactoring the code and adjust the contents in package.json

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,11 +3,14 @@ import yaml from 'js-yaml';
 import gulpRequireTasks from 'gulp-require-tasks';
 import connect from 'gulp-connect';
 
-let configuration  = yaml.load(
-            fs.readFileSync('config.yml', 'utf8')
-        );
+let configuration = yaml.load(
+    fs.readFile('config.yml', 'utf8', (err, data) => {
+        if (err)
+            throw err;
+    })
+);
 
 gulpRequireTasks({
-    path: process.cwd()+"/"+configuration.tasks.path,
-    arguments: [configuration,connect]
+    path: process.cwd() + "/" + configuration.tasks.path,
+    arguments: [configuration, connect]
 });
