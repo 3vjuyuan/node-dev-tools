01d121a59d16ef778697c4417f0cfefff6419692
Weiye Sun <weiye.sun@3vjuyuan.com>
2017-04-06 23:50:43 +0200

    [FEATURE] Add sass lint task and adapt the compile task

--- gulp-tasks/styles/compile.js
+++ gulp-tasks/styles/compile.js
@@ -1,19 +1,18 @@
 import sass from 'gulp-sass';
-import concat from 'gulp-concat';
 import sourcemaps from 'gulp-sourcemaps';
 import gulputil from 'gulp-util';
 import prefix from 'gulp-autoprefixer';
 
 module.exports = {
     dep: ['styles:lint'],
-    fn: function (gulp, configuration, connect) {
+    fn: function (gulp, configuration) {
         return gulp.src(configuration.style.path.src + '/**/*.scss')
             .pipe(sourcemaps.init())
             .pipe(sass({
                 outputStyle: 'compressed',
                 includePaths: configuration.style.path.vendors
             }).on('error',gulputil.log))
-            .pipe(prefix('last 2 version', 'ie 10'))
+            .pipe(prefix(configuration.style.autoprefix.browser, configuration.style.autoprefix.env))
             .pipe(sourcemaps.write('./'))
             .pipe(gulp.dest(configuration.style.path.compiled));
     }
