da126b3d0c7ffc4070269d1800b0d005594d4ec9
李妍 <liyan@3vjuyuan.com>
2017-04-10 15:46:14 +0800

    [TASK]add configrution

--- gulp-tasks/sprity/sprity.js
+++ gulp-tasks/sprity/sprity.js
@@ -6,18 +6,17 @@ module.exports ={
     fn:function (gulp,configuration) {
         return sprity.src({
             src:configuration.image.path.src+'/**/*.{png,jpg,gif,svg}',
-            style:'./sprity.css',
-            format:'png',
-            orientation :'binary-tree',
-            processor:'css',
-            engine:'gm',
-            name:'sprity',
-            prefix:'sprity',
+            style:configuration.image.sprity.style,
+            format:configuration.image.sprity.format,
+            orientation :configuration.image.sprity.orientation,
+            processor:configuration.image.sprity.processor,
+            engine:configuration.image.sprity.engine,
+            name:configuration.image.sprity.name,
+            prefix:configuration.image.sprity.prefix,
         })
             .on('error',function (err) {
                 console.log(err.toString());
             })
-
             .pipe(gulpif('*.png', gulp.dest(configuration.image.path.dest),
                 gulp.dest(configuration.style.path.compiled+'/sprity')))
     }
