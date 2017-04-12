d1f0fc60ffb295de98d8549783ebce311047ae88
李妍 <liyan@3vjuyuan.com>
2017-04-12 20:24:47 +0800

    [TASK]Modify svg merge and sprity images without problems

--- gulp-tasks/styles/sprity.js
+++ gulp-tasks/styles/sprity.js
@@ -1,24 +1,13 @@
-import gulpif from 'gulp-if';
-import sprity from 'sprity';
-import svgSprite from 'gulp-svg-sprite';
-import plumber from 'gulp-plumber';
-
+import concat from 'gulp-concat';
+import sourceMaps from 'gulp-sourcemaps';
 module.exports = {
+    dep:['styles:sprity-svg','styles:sprity-img'],
     fn: function (gulp, configuration) {
-         gulp.src(configuration.image.path.src + '/**/*.svg')
-            .pipe(plumber())
-            .pipe(svgSprite(configuration.image.svgsprityconfig))
-            .on('error',function (error) {
-                console.log(error.toString());
-            })
-            .pipe(gulp.dest('.'))
-
-        return sprity.src(configuration.image.sprity)
-            .on('error', function (err) {
-                console.log(err.toString());
-            })
-            .pipe(gulpif('*.png', gulp.dest(configuration.image.path.dest),
-                gulp.dest(configuration.style.path.src.css)))
+      return gulp.src(configuration.image.sprity.dest + '/**/*.scss')
+            .pipe(sourceMaps.init())
+            .pipe(concat('sprity.scss'))
+            .pipe(sourceMaps.write('./'))
+            .pipe(gulp.dest(configuration.style.path.src.sass+'/sprity'))
         
     }
 }
