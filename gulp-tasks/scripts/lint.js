019510c08c1d97dcf03c573374d8d545fc25f8cc
FanJunFang <981322807@qq.com>
2017-04-21 14:41:40 +0800

    AddEslintrc

--- gulp-tasks/scripts/lint.js
+++ gulp-tasks/scripts/lint.js
@@ -4,9 +4,6 @@ module.exports = {
     fn: function (gulp,configuration,connect) {
         return gulp.src([configuration.script.path.src+'*.js','!node_modules/**'])
             .pipe(eslint({
-                rules: {
-                    'my-custom-rule': 1
-                },
                 globals: [
                     'jQuery',
                     '$'
