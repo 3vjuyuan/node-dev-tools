58d435de4063811f3a76ebb4fc20fa02ece2abac
Weiye Sun <weiye.sun@3vjuyuan.com>
2017-04-30 19:16:18 +0200

    [FEATURE] Add merge.js to merge the detault and custom configuraions

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,12 +1,28 @@
+/**
+ * Copyright (c) 2014-present, San Wei Ju Yuan Tech Ltd. <https://www.3vjuyuan.com>
+ * All rights reserved.
+ *
+ * This file is part of node-dev-tools, licensed under the MIT license (MIT) found
+ * in the LICENSE file in the root directory of this source tree.
+ *
+ * For more details:
+ * https://www.3vjuyuan.com/licenses/mit.html
+ *
+ * @author Team fancy <fancy@3vjuyuan.com>
+ * @author Team Delta <delta@3vjuyuan.com>
+ */
+
 'use strict';
 
 import fs from 'fs';
 import yaml from 'js-yaml';
 import gulpRequireTasks from 'gulp-require-tasks';
 import connect from 'gulp-connect';
+import merge from './scrpits/merge';
 
-let configuration = yaml.load(
-    fs.readFileSync('config.yml', 'utf8')
+let configuration = merge(
+    yaml.load(fs.readFileSync('default.yml', 'utf8')),
+    yaml.load(fs.readFileSync('UserProject/config.yml', 'utf8')),
 );
 
 gulpRequireTasks({
