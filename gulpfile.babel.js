91796c3750ac06159d7eeae0e19b956653d30bb2
Weiye Sun <weiye.sun@3vjuyuan.com>
2017-05-07 19:03:34 +0200

    [TASK] Add sass lint file check

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -29,12 +29,13 @@ process.getObjectType = function (obj) {
 };
 
 let currentPath = process.cwd() + '/',
-    projectPath = process.env.PWD + '/' + (empty(process.env.npm_package_project_path) ? '' : process.trimPath(process.env.npm_package_project_path)),
+    projectPath = process.env.PWD + (empty(process.env.npm_package_project_path) ? '/' : '/' + process.trimPath(process.env.npm_package_project_path) + '/'),
     configuration = merge(
         yaml.load(fs.readFileSync(currentPath + 'default.yml', 'utf8')),
         fs.existsSync(projectPath + 'project.yml') ? yaml.load(fs.readFileSync(projectPath + 'project.yml', 'utf8')) : {}
     ),
-    lint;
+    lint,
+    lintFilePath = '';
 
 if (!Array.isArray(configuration.tasks.path)) {
     console.error("\x1b[31m", '\nThe default tasks configuration is lost. The path must be an array.\nPlease Check you configuration file in "UserProject/config.yml".');
@@ -43,7 +44,11 @@ if (!Array.isArray(configuration.tasks.path)) {
 
 if (empty(lint = configuration.styles.lint) || process.getObjectType(lint) !== 'object') {
     lint = {configFile: currentPath + '.sass-lint.yml'};
-} else if (empty(lint.configFile)) {
+} else if(
+    fs.existsSync(lintFilePath = projectPath + process.trimPath(lint.configFile)) ||
+    fs.existsSync(lintFilePath = process.env.PWD + '/' + process.trimPath(lint.configFile))){
+    lint.configFile = lintFilePath;
+} else {
     lint.configFile = currentPath + '.sass-lint.yml';
 }
 
